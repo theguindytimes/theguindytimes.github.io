980b927fe4d47249ca4b6f632c85194cb9fc212b
Mithul <mithulmathi@gmail.com>
2014-07-01 20:45:38 +0530

    gallery changes. changes news to article trending

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,12 @@ class HomeController < ApplicationController
 	def index
 		@articles = Article.all
 		@article = Article.new
-		tags = Article.tag_counts
+		tags = Article.tag_counts.order('taggings_count DESC')
 		require 'nokogiri'
         
 		# tags = ActsAsTaggableOn::Tag.order('taggings_count').all
 		@tags = []
+		@images = []
 		@numbers = {1 => 'first', 2 => 'second', 3 => 'third', 4 => 'fourth', 5 => 'fifth', 6 => 'sixth', 7 => 'seventh', 8 => 'eighth'} #Any better way of doing this ?
 		tags.limit(7).each do |t|
 			articles = []
@@ -16,9 +17,10 @@ class HomeController < ApplicationController
         		img_srcs = doc.css('img').map{ |i| i['src'] }
         		article={}
 				article['title']=a.title
-				article['content']=a.content
+				article['content']=a.content[0,400]
 				if img_srcs.length>0
-				article['img']=img_srcs[0]
+					article['img']=img_srcs[0]
+					@images << {'src' => img_srcs,'article'=> a} unless @images.include?({'src' => img_srcs,'article'=> a})
 				else
 					article['img']='default_article.jpg'
 				end
@@ -29,8 +31,8 @@ class HomeController < ApplicationController
 			end
 			@tags << {name: t.name, articles: articles}
 		end
-		@images = Ckeditor::Picture.all.pluck('id','data_file_name')
-		#render :text => @images.to_yaml
+		# @images = Ckeditor::Picture.all.pluck('id','data_file_name')
+		# render :text => @images.to_yaml
 	end
 
 end

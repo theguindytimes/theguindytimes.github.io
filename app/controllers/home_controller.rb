7bc244367d8fb4ccb32558f0b5d932c61f5bbce9
Mithul <mithulmathi@gmail.com>
2014-07-02 00:08:18 +0530

    draft preview in status field of articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,19 @@
 class HomeController < ApplicationController
 
+	before_action :old_article_fix
+
 	def index
-		@articles = Article.all
+		@articles = Article.where(status: "Visible to Public")
 		@article = Article.new
-		tags = Article.tag_counts.order('taggings_count DESC')
+		tags = Article.where(status: "Visible to Public").tag_counts.order('taggings_count DESC')
 		require 'nokogiri'
-        
 		# tags = ActsAsTaggableOn::Tag.order('taggings_count').all
 		@tags = []
 		@images = []
 		@numbers = {1 => 'first', 2 => 'second', 3 => 'third', 4 => 'fourth', 5 => 'fifth', 6 => 'sixth', 7 => 'seventh', 8 => 'eighth'} #Any better way of doing this ?
 		tags.limit(7).each do |t|
 			articles = []
-			Article.tagged_with(t).order("views").limit(4).each do |a|
+			Article.tagged_with(t).order("views").where(status: "Visible to Public").limit(4).each do |a|
 				doc = Nokogiri::HTML( a.content )
         		img_srcs = doc.css('img').map{ |i| i['src'] }
         		article={}
@@ -32,8 +33,19 @@ class HomeController < ApplicationController
 			@tags << {name: t.name, articles: articles}
 		end
 		# @images = Ckeditor::Picture.all.pluck('id','data_file_name')
-		# render :text => @images.to_yaml
-		@hotArticles = Article.order("views").limit(5)
+		@hotArticles = Article.where(status: "Visible to Public").order("views").limit(5)
+		# render :text => @articles.to_yaml
+	end
+
+	private
+
+	def old_article_fix
+		Article.all.each do |a|
+			if not (a.status == 'Visible to Public' or a.status == 'Draft/Preview')
+				a.status = 'Visible to Public'
+				a.save
+			end
+		end
 	end
 
 end

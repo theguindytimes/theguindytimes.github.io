116dce127aa3dd824afdc5f0f699782ccb89b11f
Prashant <prashant.barca@gmail.com>
2014-08-17 10:02:28 -0400

    Add analytics

--- app/mailers/feedback.rb
+++ app/mailers/feedback.rb
@@ -4,12 +4,12 @@ class Feedback < ActionMailer::Base
         @body=body
         @email=email
         @name=name
-        mail :to => email, :from => "prashant.barca@gmail.com", :subject => "TheGuindyTimes: #{subject}"
+        mail :to => email, :from => "President", :subject => "The Guindy Times: #{subject}"
     end
     def response(name,email,subject,body)
         @body=body
         @email=email
         @name=name
-        mail :to => "srravya.c@gmail.com,adnanalime@gmail.com", :from => "prashant.barca@gmail.com", :subject => "TheGuindyTimes: #{subject}"
+        mail :to => "srravya.c@gmail.com,adnanalime@gmail.com,prashant.barca@gmail.com", :from => "President", :subject => "The Guindy Times: #{subject}"
     end
 end
